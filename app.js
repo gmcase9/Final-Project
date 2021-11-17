19b02e2a003e084325367c64397a510c561c00b2
Graham <89858346+gmcase9@users.noreply.github.com>
2021-11-16 19:42:01 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var toggle=false;
+
+
+
+document.body.querySelector(".menuIcon").addEventListener(
+    "click", function (){
+
+        toggle=!toggle;
+
+        if (toggle){
+            document.body.querySelector(".menu").style.right="0";
+        }else{
+            document.body.querySelector(".menu").style.right="-150px";
+        }
+
+
+    })
